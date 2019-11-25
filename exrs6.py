ec1d9740a0af2888bb86effcbedeb887f1769406
vlysenok <tina.lysenok@gmail.com>
2019-11-25 17:28:45 +0200

    exrs from 25112019

---
+++ exrs6.py
@@ -0,0 +1,24 @@
+"""
+Socialist distribution
+"""
+
+
+def socialist_distribution(population, minimum):
+    """
+    You will be given two parameters, population and minimum: your goal is to give 
+    to each one according to his own needs (which we assume to be equal to minimum 
+    for everyone, no matter what), taking from the richest (bigger numbers) first.
+    :param population: List[int]
+    :param minimum: int
+    :return: List[int]
+    """""
+    if len(population) * minimum > sum(population):
+        return []
+
+    while min(population) < minimum:
+        i_max = population.index(max(population))
+        i_min = population.index(min(population))
+        population[i_max] -= 1
+        population[i_min] += 1
+
+    return population
