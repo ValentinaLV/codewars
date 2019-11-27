d7d1ea9b5238753fd1b5e4ba2c1d4be15b40bcce
vlysenok <tina.lysenok@gmail.com>
2019-11-27 13:44:49 +0200

    added check_coupon method

--- exrs8.py
+++ exrs8.py
@@ -1,6 +1,8 @@
 """
 The Crockford Invocation
+The Coupon Code
 """
+import datetime
 
 
 def add(a):
@@ -17,3 +19,12 @@ def multiply(a):
 
 def apply(op):
     return op
+
+
+def check_coupon(entered_code, correct_code, current_date, expiration_date):
+    current_date_parse = datetime.datetime.strptime(current_date, '%B %d, %Y')
+    expiration_date_parse = datetime.datetime.strptime(expiration_date, '%B %d, %Y')
+    if entered_code is correct_code:
+        return current_date_parse <= expiration_date_parse
+    else:
+        return False
